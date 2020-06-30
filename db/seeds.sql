f336a9895a0e9041b24ef37f5f4f1c7c24bcefb9
Tyler McBride <tylerdb@Tylers-MacBook-Pro-2.local>
2020-06-30 13:37:53 -0700

    DB Setup

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Burger', true);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Classic Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Classic Burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
