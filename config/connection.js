4eaa51d11dff053dc97764a9d443c38d63bcf30a
Tyler McBride <tylerdb@Tylers-MacBook-Pro-2.local>
2020-07-01 16:10:20 -0700

    JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,9 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -8,6 +11,8 @@ var connection = mysql.createConnection({
   password: process.env.MYSQL_PASSWORD,
   database: "burgers_db"
 });
+
+}
 
 // Make connection.
 connection.connect(function(err) {
